b98abb64cbc058e5e07eb946524098d45505744c
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2024-11-05 20:10:41 +0200

    chore: code cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,100 +1,38 @@
-use std::result::Result;
-use zed_extension_api::{lsp::{Completion, Symbol}, Command as ZedCommand, Extension, Worktree};
+use zed_extension_api::{
+    self as zed,
+    SlashCommand, SlashCommandOutput, Worktree,
+};
+use webbrowser;
 
-struct OpenCodeiumSite;
+struct CodeiumExtension {}
 
-impl OpenCodeiumSite {
-    fn execute(&self) -> Result<(), String>{
-        // Open codeium site in the browser
-        webbrowser::open("https://codeium.com")
-        .map_err(|_| "Failed to open the browser.".to_string())
-    }
-}
-
-struct CodeiumExtension;
-
-impl CodeiumExtension{
-    fn new() -> Self {
-        // Triggers the codeium extension
-        OpenCodeiumSite.execute().unwrap();
-        CodeiumExtension
-    }
-}
-
-impl Extension for CodeiumExtension {
-    fn language_server_command(
-        &mut self,
-        _language_server_id: &zed_extension_api::LanguageServerId,
-        _worktree: &Worktree,
-    ) -> zed_extension_api::Result<ZedCommand> {
-        Err("`language_server_command` not implemented".to_string())
-    }
-
-    fn language_server_initialization_options(
-        &mut self,
-        _language_server_id: &zed_extension_api::LanguageServerId,
-        _worktree: &Worktree,
-    ) -> zed_extension_api::Result<Option<zed_extension_api::serde_json::Value>> {
-        Ok(None)
-    }
-
-    fn language_server_workspace_configuration(
-        &mut self,
-        _language_server_id: &zed_extension_api::LanguageServerId,
-        _worktree: &Worktree,
-    ) -> zed_extension_api::Result<Option<zed_extension_api::serde_json::Value>> {
-        Ok(None)
-    }
-
-    fn label_for_completion(
-        &self,
-        _language_server_id: &zed_extension_api::LanguageServerId,
-        _completion: Completion,
-    ) -> Option<zed_extension_api::CodeLabel> {
-        None
-    }
-
-    fn label_for_symbol(
-        &self,
-        _language_server_id: &zed_extension_api::LanguageServerId,
-        _symbol: Symbol,
-    ) -> Option<zed_extension_api::CodeLabel> {
-        None
-    }
-
-    fn complete_slash_command_argument(
-        &self,
-        _command: zed_extension_api::SlashCommand,
-        _args: Vec<String>,
-    ) -> zed_extension_api::Result<Vec<zed_extension_api::SlashCommandArgumentCompletion>, String> {
-        Ok(Vec::new())
-    }
-
+impl zed::Extension for CodeiumExtension {
     fn run_slash_command(
         &self,
-        _command: zed_extension_api::SlashCommand,
+        command: SlashCommand,
         _args: Vec<String>,
         _worktree: Option<&Worktree>,
-    ) -> zed_extension_api::Result<zed_extension_api::SlashCommandOutput, String> {
-        Err("`run_slash_command` not implemented".to_string())
+    ) -> Result<SlashCommandOutput, String> {
+        match command.name.as_str() {
+            "open-codeium" => {
+                let url = "https://codeium.com";
+                if webbrowser::open(url).is_ok() {
+                    Ok(SlashCommandOutput {
+                        text: "Codeium opened".to_string(),
+                        sections: vec![],
+                    })
+                } else {
+                    Err("Failed to open Codeium".to_string())
+                }
+            }
+            _ => Err(format!("Unknown command: {}", command.name)),
+        }
     }
 
-    fn suggest_docs_packages(&self, _provider: String) -> zed_extension_api::Result<Vec<String>, String> {
-        Ok(Vec::new())
-    }
-
-    fn index_docs(
-        &self,
-        _provider: String,
-        _package: String,
-        _database: &zed_extension_api::KeyValueStore,
-    ) -> zed_extension_api::Result<(), String> {
-        Err("`index_docs` not implemented".to_string())
-    }
-    
     fn new() -> Self
     where
-        Self: Sized {
-        todo!()
+        Self: Sized,
+    {
+        CodeiumExtension {}
     }
 }
